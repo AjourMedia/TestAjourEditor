ef2af482e00269a4393c6718744c8985ee21dd41
AjourMedia <morten.ellingsen@ajourmedia.no>
2025-04-24 03:40:27 +0200

    v1.4.34

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 using Ajour.EditorLib;
 
 namespace TestAjourEditor
@@ -10,7 +11,8 @@ namespace TestAjourEditor
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
+				.UseMauiCommunityToolkit()
+				.ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
